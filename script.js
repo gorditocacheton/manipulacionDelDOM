a006b9676cc5cca448a53540c66507a4b9206849
gorditocacheton <112261766+gorditocacheton@users.noreply.github.com>
2022-09-07 18:23:21 -0500

    agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('click' , sumarInputValue);
+
+
+function sumarInputValue(e) {
+    console.log(e);
+    e.preventDefault();
+  const sumaInputs = input1.value + input2.value;
+  pResult.innerText = "Resultado: " + sumaInputs ;
+}
